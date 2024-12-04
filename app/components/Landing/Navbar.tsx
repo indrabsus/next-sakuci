39a0fc2dd8e714a686802e584130296293937914
indrabsus <bataravix3r@gmail.com>
2024-12-05 05:25:41 +0700

    daisy ui

--- app/components/Landing/Navbar.tsx
+++ app/components/Landing/Navbar.tsx
@@ -1,16 +1,59 @@
 export default function Navbar() {
     return (
-        <div className="bg-gray-800 h-12">
-            <div className="max-w-screen-xl mx-auto flex items-center justify-between h-full lg:mx-5 md:mx-5 sm:mx-5">
-            <div className="text-white text-2xl font-bold">SMK Sangkuriang 1 Cimahi</div>
-            <div className="text-white">
-                <ul className="flex gap-5">
-                    <li>Home</li>
-                    <li>Kontak</li>
-                    <li>Tentang Kami</li>
-                </ul>
-            </div>
-            </div>
-        </div>
+        
+
+<div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Item 1</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">Button</a>
+  </div>
+</div>
+
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+
 import Navbar from "./components/Landing/Navbar";
+
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Home</h1>
     </div>
-  )
+  );
 }
